48b9291145c68e2a6eae073057b7380a8be99b3a
Jyunta Suzuki <s23C1074ZE@s.chibakoudai.jp>
2024-11-14 03:52:33 +0900

    Revert to normal behavior and remove error input

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 Jyunta Suzuki
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -11,6 +11,15 @@ res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = "15" ] || ng "$LINENO"
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)           #計算できない値を入力してみる
+[ "$?" = 1 ]      || ng "$LINENO" #終了ステータスが1なのを確認
+[ "${out}" = "" ] || ng "$LINENO" #この行と上の行は入れ替えるとダメです
+  　                                      #（↑なぜかは考えてみましょう）
+out=$(echo | ./plus)              #なにも入力しない
+[ "$?" = 1 ]      || ng "$LINENO" #これも異常終了する
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK  # 通ったのが（人間に）分かるように表示
 exit $res
