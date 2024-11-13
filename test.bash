e8cca05849303789b8a670e7a501813ea2677aa4
Jyunta Suzuki <s23C1074ZE@s.chibakoudai.jp>
2024-11-14 03:27:38 +0900

    Revert to normal behavior and remove error input

--- test.bash
+++ test.bash
@@ -1,14 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2024 Jyunta Suzuki
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
     echo "${1}行目が違うよ"
-    res=1  # 追加
+    res=1
 }
 
 res=0
-a="山田"
-[ "$a" = "上田" ] || ng "$LINENO"  # LINENOは，この行の行番号の入る変数
-[ "$a" = "山田" ] || ng "$LINENO"  # ngに第一引数として$LINENOを付与
 
-exit $res  # このシェルスクリプトの終了ステータスを返して終了
+out=$(seq 5 | ./plus)
+[ "${out}" = "15" ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK  # 通ったのが（人間に）分かるように表示
+exit $res
 
